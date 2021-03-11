e4641606514e8250f052f3f8627eb098aa0be7de
Mario Sever <34382956+mariosever@users.noreply.github.com>
2021-03-11 09:02:10 +0100

    Add files via upload

--- front-page.php
+++ front-page.php
@@ -2,8 +2,23 @@
 
 <?php get_header(); ?>
 
+<!-- header -->
+<header>
+	<div class="container">
+		<div class="header--inner">
+			<h1 class="header--title">
+				Hrvatska komora <span class="orange">socijalnih pedagoga</span>
+			</h1>
+			<div class="header--cta">
+				<a href="/o-nama" class="header--link">Više o nama</a>
+			</div>
+		</div>
+		<div class="header--image">
+			<img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/ruke.jpg">
+		</div>
+	</div>
+</header>
 
-<h1>Naslovnica</h1>
 
 
 
--- header.php
+++ header.php
@@ -5,6 +5,40 @@
 	<title><?php wp_title(); ?></title>	
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Red+Hat+Text:wght@400;500;700&display=swap" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+
+<!-- topbar -->
+<div class="topbar">
+	<div class="container">
+		<span class="email"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/email.svg"> hk.socijalnih.pedagoga@gmail.com</span>
+		<span class="location"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/lokacija.svg"> Ulica sv. Mateja 70/a, Dugave 10000 Zagreb</span>
+	</div>
+</div>
+
+<!-- navigacija -->
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="<?php echo get_option('siteurl'); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/hksp_logo.svg" alt="HKSP"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="main-menu">
+            <?php
+            wp_nav_menu(array(
+                'theme_location' => 'main-menu',
+                'container' => false,
+                'menu_class' => '',
+                'fallback_cb' => '__return_false',
+                'items_wrap' => '<ul id="%1$s" class="navbar-nav me-auto mb-2 mb-md-0 %2$s">%3$s</ul>',
+                'depth' => 2,
+                'walker' => new bootstrap_5_wp_nav_menu_walker()
+            ));
+            ?>
+        </div>
+    </div>
+</nav>
