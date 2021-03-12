d847756fea28d6147c0347aee346901701fc4252
Mario Sever <34382956+mariosever@users.noreply.github.com>
2021-03-12 11:35:30 +0100

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,21 @@
+<?php 
+/*
+Template Name: Blog arhiva
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="main">
+
+	<div class="container">
+
+		<h2 class="section--title">Arhiva vijesti</h2>
+
+		
+	</div>
+
+</div>
+
+
+<?php get_footer(); ?>
--- front-page.php
+++ front-page.php
@@ -7,17 +7,116 @@
 	<div class="container">
 		<div class="header--inner">
 			<h1 class="header--title">
-				Hrvatska komora <span class="orange">socijalnih pedagoga</span>
+				Hrvatska komora <br><span class="orange">socijalnih pedagoga</span>
 			</h1>
 			<div class="header--cta">
-				<a href="/o-nama" class="header--link">Više o nama</a>
+				<a href="o-nama/" class="header--link">Više o nama</a>
 			</div>
-		</div>
-		<div class="header--image">
-			<img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/ruke.jpg">
 		</div>
 	</div>
 </header>
+
+<!-- boxevi -->
+<div class="boxevi">
+	<div class="container">
+		<div class="row row-cols-1 row-cols-lg-3">
+			<div class="col mb-4">
+				<a href="clanstvo/">
+					<div class="card">
+						<div class="card-body">
+							<span class="material-icons">people_outline</span>
+							<span class="icon-text">Postani član</span>
+						</div>
+					</div>
+				</a>
+			</div>
+
+			<div class="col mb-4">
+				<a href="edukacije/">
+					<div class="card">
+						<div class="card-body">
+							<span class="material-icons">school</span>
+							<span class="icon-text">Edukacije za članove</span>
+						</div>
+					</div>
+				</a>
+			</div>
+
+			<div class="col mb-4">
+				<a href="prijava/">
+					<div class="card">
+						<div class="card-body">
+							<span class="material-icons">login</span>
+							<span class="icon-text">Prijava u sustav</span>
+						</div>
+					</div>
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- news -->
+<div class="main">
+
+	<div class="container">
+
+		<h2 class="section--title">Novosti</h2>
+
+		<div class="row row-cols-1 row-cols-md-3 g-4 blog-grid">
+
+			<?php 
+			
+			$args = array( 'category_name' => 'novosti', 
+							'posts_per_page' => 9 
+							); 
+				
+			$the_query = new WP_Query( $args ); 
+				
+			if ( $the_query->have_posts() ) : 
+					
+				while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+					<?php $post_link = get_permalink(); ?>
+
+					<div class="col">
+    					<div class="card h-100">
+
+							<a href="<?php echo $post_link; ?>">	
+								<?php the_post_thumbnail( 'full', array( 'class'=>'card-img-top img-fluid' ) ); ?>
+							</a>
+
+							<div class="card-body">
+								<div class="card-meta">
+									<div class="author">
+										Objavio: <?php the_author(); ?> 
+									</div>
+									<div class="date">
+										Datum: <?php echo get_the_date(); ?>
+									</div>
+								</div>
+								<a href="<?php echo $post_link; ?>"><h5 class="card-title"><?php the_title(); ?></h5></a>
+								<p class="card-text"><?php the_excerpt(); ?></p>
+								<a href="<?php echo $post_link; ?>" class="card-link">Opširnije...</a>
+							</div>
+
+						</div>
+					</div>
+
+				<?php endwhile; 
+			else: 
+			
+				_e( 'Nažalost nema objava u ovoj kategoriji.', 'textdomain' ); 
+			endif; 
+				
+			wp_reset_postdata(); 
+			?>
+
+		</div><!-- row -->
+
+	</div>
+
+</div>
 
 
 
